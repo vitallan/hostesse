56c7ec52f166efb85a51c451cf4f07673a9d23a1
Leandro Facchinetti <leafac@gmail.com>
2012-10-25 20:30:20 -0200

    Start include in SimpleTemplateEngine

--- spec/unit/simple_template_engine_spec.rb
+++ spec/unit/simple_template_engine_spec.rb
@@ -2,36 +2,49 @@ require 'spec_helper.rb'
 
 describe Hostesse::SimpleTemplateEngine do
 
-  subject { Hostesse::SimpleTemplateEngine.new('spec/support/simple_hosts') }
+  subject { Hostesse::SimpleTemplateEngine.new(base_path) }
 
-  let(:simple_hosts_filename)          { 'localhost' }
+  let(:base_path)                      { 'spec/support' }
+
+  let(:simple_hosts_filename)          { 'simple' }
   let(:simple_hosts_complete_filename) { complete_filename(simple_hosts_filename) }
 
-  it "shouldn't change a simple file" do
+  describe 'simple hosts' do
 
-    subject.parse(simple_hosts_filename).should match '127.0.0.1 localhost'
+    it "shouldn't change a simple file" do
+
+      subject.parse(simple_hosts_filename).should match '127.0.0.1 localhost'
+
+    end
+
+    it 'should prepend the filename as a comment' do
+
+      subject.parse(simple_hosts_filename).should match "# #{ simple_hosts_complete_filename }"
+
+    end
+
+    it 'should memoize the result' do
+
+      first_result = subject.parse(simple_hosts_filename)
+
+      File.stub(:read).with(simple_hosts_complete_filename).and_raise("it shouldn't need to read file again")
+
+      subject.parse(simple_hosts_filename).should == first_result
+
+    end
 
   end
 
-  it 'should prepend the filename as a comment' do
+  describe 'includes' do
 
-    subject.parse(simple_hosts_filename).should match "# #{ simple_hosts_complete_filename }"
-
-  end
-
-  it 'should memoize the result' do
-
-    first_result = subject.parse(simple_hosts_filename)
-
-    File.stub(:read).with(simple_hosts_complete_filename).and_raise("it shouldn't need to read file again")
-
-    subject.parse(simple_hosts_filename).should == first_result
+    let(:include_hosts_filename)          { 'include' }
+    let(:include_hosts_complete_filename) { complete_filename(include_hosts_filename) }
 
   end
 
   private
 
     def complete_filename(filename)
-      File.expand_path("spec/support/simple_hosts/#{ filename }.hosts")
+      File.expand_path("#{ base_path }/#{ filename }.hosts")
     end
 end
