b232e8f6dca2a45359adae3c78932905d615df2f
Leandro Facchinetti <leafac@gmail.com>
2012-12-03 12:12:15 -0200

    Bump version

--- lib/hostesse/version.rb
+++ lib/hostesse/version.rb
@@ -1,5 +1,5 @@
 require 'hostesse/environment'
 
 module Hostesse
-  VERSION = '0.1.2' + (Hostesse::Environment.is_jruby? ? '-java' : '')
+  VERSION = '0.1.3' + (Hostesse::Environment.is_jruby? ? '-java' : '')
 end
